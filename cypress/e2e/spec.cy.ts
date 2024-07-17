b6296032cab0b07f6dd65d381113ea065e74944e
kawashimatakuya117 <t.kawashima17@gmai.com>
2024-07-17 14:17:09 +0900

    feat(app/pages/top): TOPページ作成

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,5 @@
+describe('template', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000')
+  })
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { Box } from "@kuma-ui/core";
+import { Top } from './pages/top'
 
-export const ThisIsTheBox = () => {
-  return (
-    <Box p={8} bg="blue" color="white">
-      Hello world
-    </Box>
-  );
-};
-
-export default ThisIsTheBox
+export default function Page(){
+    return <Top/>
+}
