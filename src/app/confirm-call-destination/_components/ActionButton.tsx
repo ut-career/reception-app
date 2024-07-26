8038bc4c85a08dce753650d6a585c3a06e69079d
kawashimatakuya117 <t.kawashima17@gmai.com>
2024-07-26 13:59:06 +0900

    refactor(ActionButton.tsx): コードの意図を示すコメントを追記

--- src/app/confirm-call-destination/_components/ActionButton.tsx
+++ src/app/confirm-call-destination/_components/ActionButton.tsx
@@ -34,10 +34,11 @@ const useSendMessage = (endpoint:string) =>{
       method: 'POST',
     })
 
+    //別の画面でエラーメッセージとしてアラートで表示する予定のためreturnしています。
     if (!response.ok) {
       return NextResponse.json({ error: 'Failed to send message', status: response.status })
     }
-
+    //レスポンスが200なのか401なのかによって呼び出し完了画面と呼び出し失敗画面のどちらに遷移するかを決めるためreturnしています。
     const data = await response.json()
     return data
   }
